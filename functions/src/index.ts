2ca680cc2a16c051a3148718b09f01ebdbaf774e
Paul Maier <contact@paulmaier.online>
2025-09-26 01:06:45 +0200

    Lap Deletion via firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import {logger} from "firebase-functions";
 import {HttpsError, onCall} from "firebase-functions/v2/https";
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
-import {onDocumentDeleted} from "firebase-functions/v2/firestore";
 
 initializeApp();
 
@@ -81,8 +80,10 @@ export const createLap = onCall(
         const newLap = {
           runnerId: runner.id,
           createdAt: now,
-          name: runner.name,
-          number: runner.number,
+          runnerData: {
+            name: runner.name,
+            number: runner.number,
+          },
         };
 
         // Add the new lap
@@ -90,7 +91,7 @@ export const createLap = onCall(
         transaction.set(newLapRef, newLap);
 
         // Update the runner
-        transaction.update(runnerRef, {
+        await transaction.update(runnerRef, {
           lastLapCreatedAt: now,
           laps: (runnerDoc.data()?.laps || 0) + 1,
         });
@@ -123,42 +124,63 @@ export const createLap = onCall(
   }
 );
 
-export const resetLastLapCreatedAt = onDocumentDeleted(
-  "laps/{lapId}",
-  async (event) => {
+export const deleteLap = onCall(
+  {
+    region: "europe-west1",
+    minInstances: 0, // FIXME: Set to 0 after release
+    maxInstances: 10,
+  },
+  async (request) => {
+    // Check if the user is authenticated
+    // Check if the user is authenticated
+    if (!request.auth || request.auth.token.role !== "assistant") {
+      throw new HttpsError("unauthenticated", "Authentication required.");
+    }
+
+    // Ensure the request contains the 'lapId' field
+    const lapId = request.data.lapId as string;
+    if (!lapId) {
+      throw new HttpsError("invalid-argument", "Missing field: lapId");
+    }
+
     const firestore = getFirestore();
-    const snapshot = event.data;
 
-    if (!snapshot) {
-      return;
-    }
-    const data = snapshot.data();
-
-    const runnerId = data.runnerId;
-
-    if (!runnerId) {
-      return;
-    }
-
-    const runnerRef = firestore.doc(`runners/${runnerId}`);
+    const lapRef = firestore.doc(`laps/${lapId}`);
 
     try {
       await firestore.runTransaction(async (transaction) => {
-        const runnerDoc = await transaction.get(runnerRef);
+        const lapDoc = await transaction.get(lapRef);
 
-        if (!runnerDoc.exists) {
+        if (!lapDoc.exists) {
+          throw new HttpsError("not-found", "Runde nicht gefunden.");
+        }
+
+        const runnerId = lapDoc.data()?.runnerId;
+
+        if (!runnerId) {
+          await transaction.delete(lapRef);
           return;
         }
 
-        transaction.update(runnerRef, {
+        const runnerRef = firestore.doc(`runners/${runnerId}`);
+        const runnerDoc = await transaction.get(runnerRef);
+
+        if (!runnerDoc.exists) {
+          throw new HttpsError("not-found", "Läufer nicht gefunden.");
+        }
+
+        await transaction.delete(lapRef);
+        await transaction.update(runnerRef, {
           lastLapCreatedAt: null,
-          laps: ((runnerDoc.data()?.laps || 0) - 1) >= 0 ?
-            (runnerDoc.data()?.laps || 0) - 1 :
-            0,
+          laps:
+            (runnerDoc.data()?.laps || 0) - 1 >= 0 ?
+              (runnerDoc.data()?.laps || 0) - 1 :
+              0,
         });
       });
     } catch (err) {
       logger.error(err);
+      throw new HttpsError("internal", "Internal server error");
     }
   }
 );
@@ -180,6 +202,8 @@ export const createRunner = onCall(
       throw new HttpsError("invalid-argument", "Missing field: name");
     }
 
+    const email = request.data.email as string | undefined;
+
     const firestore = getFirestore();
 
     const newestRunnerRef = firestore
@@ -188,27 +212,35 @@ export const createRunner = onCall(
       .limit(1);
 
     try {
-      const newRunner = await firestore.runTransaction(
-        async (transaction) => {
-          const newestRunnerDoc = await transaction.get(newestRunnerRef);
+      const newRunner = await firestore.runTransaction(async (transaction) => {
+        const newestRunnerDoc = await transaction.get(newestRunnerRef);
 
-          const newNumber = newestRunnerDoc.empty ?
-            1 :
-            newestRunnerDoc.docs[0].data().number + 1;
+        const newNumber = newestRunnerDoc.empty ?
+          1 :
+          newestRunnerDoc.docs[0].data().number + 1;
 
-          const newRunner = {
-            name,
-            number: newNumber,
-            type: "guest",
-            laps: 0,
-          };
+        let newRunner: {
+          name: string;
+          number: number;
+          type: string;
+          laps: number;
+          email?: string;
+        } = {
+          name,
+          number: newNumber,
+          type: "guest",
+          laps: 0,
+        };
 
-          const newRunnerRef = firestore.collection("runners").doc();
-          transaction.set(newRunnerRef, newRunner);
-
-          return newRunner;
+        if (email) {
+          newRunner = {...newRunner, email};
         }
-      );
+
+        const newRunnerRef = firestore.collection("runners").doc();
+        transaction.set(newRunnerRef, newRunner);
+
+        return newRunner;
+      });
 
       return newRunner;
     } catch (err) {
