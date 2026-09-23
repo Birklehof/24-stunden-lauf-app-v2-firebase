3d7ffa9e8b0a3244f3e5aa366371c59f5ff402bf
Paul Maier <github@paulmaier.online>
2026-09-23 22:18:24 +0200

    Upgrade dependencies

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,7 +22,7 @@ class LapTooEarlyError extends Error {
 export const createLap = onCall(
   {
     region: "europe-west3",
-    minInstances: 5, // FIXME: Set to 0 after release
+    minInstances: 5, // FIXME: Set back to 0 after event
     maxInstances: 10,
   },
   async (request) => {
@@ -127,7 +127,7 @@ export const createLap = onCall(
 export const deleteLap = onCall(
   {
     region: "europe-west3",
-    minInstances: 2, // FIXME: Set to 0 after release
+    minInstances: 2, // FIXME: Set back to 0 after event
     maxInstances: 10,
   },
   async (request) => {
